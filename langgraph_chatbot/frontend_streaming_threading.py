4665052dc86e9a896d044e12e1da41fb50e18956
mansagupta <nymansa.gupta@gmail.com>
2026-06-26 22:58:06 +0530

    tool integration

--- langgraph_chatbot/frontend_streaming_threading.py
+++ langgraph_chatbot/frontend_streaming_threading.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langgraph_tool_backend import chatbot
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 import uuid
 
 # utility functions
